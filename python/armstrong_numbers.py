04745a0cf37f54921e432a1f7add39b8a098c416
robertuptc <robertuptc@gmail.com>
2024-06-05 17:09:39 -0400

    python problem finished

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,12 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    answer_arr = []
+    for i in numbers:
+        list_of_nums = list(str(i))
+        result = 0
+        for j in list_of_nums:
+            power_of = int(j) ** len(list_of_nums)
+            result += power_of
+        if result == i:
+            answer_arr.append(result)
+    return answer_arr
